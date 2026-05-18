f3ba6ebf780b92ab77bc954dd649166c111b8f7f
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-19 07:04:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,38 @@
-import express from 'express';
-import http from 'node:http';
-import { createBareServer } from "@tomphttp/bare-server-node";
-import cors from 'cors';
-import path from 'node:path';
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { createBareServer } = require('@tomphttp/bare-server-node');
+
+const app = express();
 
 const server = http.createServer();
-const app = express();
-const __dirname = process.cwd();
-const PORT = 8080;
 
-const bareServer = createBareServer('/riku-backend/');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+const bare = createBareServer('/bare/');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uv-core', express.static(path.join(__dirname, 'uv')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'gate.html'));
-});
-
-app.get('/session', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'main.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 server.on('request', (req, res) => {
-    if (bareServer.shouldRoute(req)) {
-        bareServer.routeRequest(req, res);
-    } else {
-        app(req, res);
-    }
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
 });
 
 server.on('upgrade', (req, socket, head) => {
-    if (bareServer.shouldRoute(req)) {
-        bareServer.routeUpgrade(req, socket, head);
-    } else {
-        socket.end();
-    }
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
 });
 
-server.listen({ port: PORT });
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`);
+});
